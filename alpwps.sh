fcc8142fc07b6af833162c7f4c1fb50d7ef860e3
Alp <135449713+Alp19@users.noreply.github.com>
2023-06-04 01:05:47 +0300

    Add files via upload

---
+++ alpwps.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+echo " 
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⡀⠒⠒⠦⣄⡀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⢀⣤⣶⡾⠿⠿⠿⠿⣿⣿⣶⣦⣄⠙⠷⣤⡀⠀⠀⠀⠀
+⠀⠀⠀⣠⡾⠛⠉⠀⠀⠀⠀⠀⠀⠀⠈⠙⠻⣿⣷⣄⠘⢿⡄⠀⠀⠀
+⠀⢀⡾⠋⠀⠀⠀⠀⠀⠀⠀⠀⠐⠂⠠⢄⡀⠈⢿⣿⣧⠈⢿⡄⠀⠀
+⢀⠏⠀⠀⠀⢀⠄⣀⣴⣾⠿⠛⠛⠛⠷⣦⡙⢦⠀⢻⣿⡆⠘⡇⠀⠀
+⠀⠀⠀⠀⡐⢁⣴⡿⠋⢀⠠⣠⠤⠒⠲⡜⣧⢸⠄⢸⣿⡇⠀⡇⠀⠀
+⠀⠀⠀⡼⠀⣾⡿⠁⣠⢃⡞⢁⢔⣆⠔⣰⠏⡼⠀⣸⣿⠃⢸⠃⠀⠀    https://github.com/Alp19
+⠀⠀⢰⡇⢸⣿⡇⠀⡇⢸⡇⣇⣀⣠⠔⠫⠊⠀⣰⣿⠏⡠⠃⠀⠀⢀
+⠀⠀⢸⡇⠸⣿⣷⠀⢳⡈⢿⣦⣀⣀⣀⣠⣴⣾⠟⠁⠀⠀⠀⠀⢀⡎
+⠀⠀⠘⣷⠀⢻⣿⣧⠀⠙⠢⠌⢉⣛⠛⠋⠉⠀⠀⠀⠀⠀⠀⣠⠎⠀
+⠀⠀⠀⠹⣧⡀⠻⣿⣷⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⡾⠃⠀⠀
+⠀⠀⠀⠀⠈⠻⣤⡈⠻⢿⣿⣷⣦⣤⣤⣤⣤⣤⣴⡾⠛⠉⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠈⠙⠶⢤⣈⣉⠛⠛⠛⠛⠋⠉⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+"
+echo "                 ALP WEB SİTE IP ADRESS AND PORT SCANNER  "
+echo "--------------------------------------------------------------"
+# Web sitesinin IP adresini bul
+echo -n "Web Site Adress: "
+read website
+
+ip_address=$(ping -c 1 $website | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}' | tail -1)
+
+if [ -z "$ip_address" ]; then
+    echo "IP adress not found."
+    exit 1
+fi
+
+echo "Web site IP adress: $ip_address"
+
+# Portları tara ve sonucu dosyaya kaydet
+echo "Scanning for open ports..."
+for port in {1..65535}; do
+    (echo >/dev/tcp/$ip_address/$port) >/dev/null 2>&1 && echo $port >> open_ports.txt
+done
+
+echo "Open Ports:"
+cat open_ports.txt
+
+echo "Open Ports 'open_ports.txt' saved."
